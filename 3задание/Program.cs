a3ac0841f49df79b0530fddca56362e09e8da193
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-09-11 14:44:49 +0300

    Добавьте файлы проекта.

---
+++ 3задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+internal class Program2
+{
+    private static void Main(string[] args)
+    {
+        var myProcess = from proc in Process.GetProcesses(".")
+                        orderby proc.BasePriority
+                        select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n");
+        foreach (var p in myProcess)
+        {
+            Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+        }
+    }
+}
